3bc392de4a368aa3743f46f394ee60caf26c6079
Andrew-P-Albanese <astr0nautj0nesapa@gmail.com>
2020-10-10 20:36:08 -0400

    added space for code page

---
+++ SumOfAllFearsSite/Scripts/custom.js
@@ -0,0 +1,27 @@
+// Custom JS
+
+document.getElementById("submitBtn").addEventListener("click", function () {
+    let inputVal = parseInt(document.getElementById("inputNum"));
+
+    const givenArr = [10, 15, 3, 7];
+
+    let outputMessage = "";
+
+    for (let loop = 0; loop < givenArr.length - 1; loop++) {
+        //any number larger than K ignore
+        if (givenArr[loop] > inputVal)
+            continue;
+
+        let solution = inputVal - givenArr[loop]; //if inputVal = 11 and number = 1 we have to find 10
+
+        if (givenArr.indexOf(solution, loop + 1) > -1) {
+            outputMessage = `A solution was found for a K value of ${inputVal}.`;
+            break;
+        }
+        else {
+            outputMessage = `No solution for a K value of ${inputVal} was found`;
+        }
+    }
+
+    document.getElementById("resultSpan").innerHTML = outputMessage;
+});
