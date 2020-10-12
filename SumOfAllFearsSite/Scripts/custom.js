cbc15c9e255200bc758f10c063884cdb1be98e51
Andrew-P-Albanese <astr0nautj0nesapa@gmail.com>
2020-10-12 09:57:16 -0400

    added working logic for the algorithm

--- SumOfAllFearsSite/Scripts/custom.js
+++ SumOfAllFearsSite/Scripts/custom.js
@@ -4,25 +4,29 @@ document.getElementById("submitBtn").addEventListener("click", function () {
 
     let inputVal = Number(document.getElementById("inputNum").value);
 
-    const givenArr = [10, 15, 3, 7];
+    if (inputVal == 0 || inputVal == 1 | inputVal == 2) {
+        document.getElementById("resultSpan").innerHTML = `No solution for a value of ${inputVal} was found.`;
+    } else { 
+        const givenArr = [10, 15, 3, 7];
 
-    let outputMessage = "";
+        let outputMessage = "";
 
-    for (let loop = 0; loop < givenArr.length - 1; loop++) {
-        //any number larger than K ignore
-        if (givenArr[loop] > inputVal)
-            continue;
+        for (let loop = 0; loop < givenArr.length - 1; loop++) {
+            //any number larger than K ignore
+            if (givenArr[loop] > inputVal)
+                continue;
 
-        let solution = inputVal - givenArr[loop]; //if inputVal = 11 and number = 1 we have to find 10
+            let solution = inputVal - givenArr[loop]; //if inputVal = 11 and number = 1 we have to find 10
 
-        if (givenArr.indexOf(solution, loop + 1) > -1) {
-            outputMessage = `A solution was found for a value of ${inputVal}.`;
-            break;
+            if (givenArr.indexOf(solution, loop + 1) > -1) {
+                outputMessage = `A solution was found for a value of ${inputVal}.`;
+                break;
+            }
+            else {
+                outputMessage = `No solution for a value of ${inputVal} was found.`;
+            }
         }
-        else {
-            outputMessage = `No solution for a value of ${inputVal} was found.`;
-        }
+
+        document.getElementById("resultSpan").innerHTML = outputMessage;
     }
-
-    document.getElementById("resultSpan").innerHTML = outputMessage;
 });
